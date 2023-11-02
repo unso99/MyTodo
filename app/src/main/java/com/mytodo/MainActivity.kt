6f8461d6700565ebf5d8986a573f1c989e56dbed
unso99 <unso99@naver.com>
2023-11-02 22:30:11 +0900

    :sparkles: @HiltAndroidApp 어노테이션 추가

--- app/src/main/java/com/mytodo/MainActivity.kt
+++ app/src/main/java/com/mytodo/MainActivity.kt
@@ -2,10 +2,17 @@ package com.mytodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mytodo.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.view = this
     }
 }
