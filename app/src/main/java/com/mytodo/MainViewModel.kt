2d4ee64e8440aba12245b1dbef1973f0018a7ea3
kimgdongju <unso99@gamil.com>
2023-11-08 17:55:51 +0900

    :sparkles: MainViewModel 파일 추가 및 기능 추가

---
+++ app/src/main/java/com/mytodo/MainViewModel.kt
@@ -0,0 +1,40 @@
+package com.mytodo
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mytodo.model.TodoEntity
+import com.mytodo.repository.TodoRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val todoRepository: TodoRepository
+) : ViewModel() {
+
+    //stateIn을 사용해서 비동기 작업을 수행하고 작업결과를 변환
+    val todoList = todoRepository.loadTodoList()
+        .stateIn(
+            initialValue = emptyList(),
+            started = SharingStarted.WhileSubscribed(5000),
+            scope = viewModelScope
+        )
+
+    fun deleteItem(item: TodoEntity) {
+        viewModelScope.launch(Dispatchers.IO) {
+            todoRepository.delete(item)
+        }
+    }
+
+    fun updateItem(item: TodoEntity) {
+        viewModelScope.launch(Dispatchers.IO) {
+            todoRepository.modify(item)
+        }
+    }
+
+
+}
